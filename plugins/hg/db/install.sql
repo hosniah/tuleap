1753821dfa5171ed4468fff751f9dea806786b48
Ahmed HOSNI <ahmed.hosni@st.com>
2012-12-04 19:36:22 +0100

    Clean up mercurial DB scripts

--- plugins/hg/db/install.sql
+++ plugins/hg/db/install.sql
@@ -1,7 +1,7 @@
-DROP TABLE IF EXISTS plugin_git;
+DROP TABLE IF EXISTS plugin_hg;
 
 
-CREATE TABLE IF NOT EXISTS `plugin_git` (
+CREATE TABLE IF NOT EXISTS `plugin_hg` (
   `repository_id` int(10) unsigned NOT NULL auto_increment,
   `repository_name` varchar(255) NOT NULL,
   `repository_description` text,
@@ -19,20 +19,20 @@ CREATE TABLE IF NOT EXISTS `plugin_git` (
 
 -- Enable service for project 100
 INSERT INTO service(group_id, label, description, short_name, link, is_active, is_used, scope, rank) 
-       VALUES      ( 100, 'plugin_git:service_lbl_key', 'plugin_git:service_desc_key', 'git', '/plugins/git/?group_id=$group_id', 1, 0, 'system', 230 );
+       VALUES      ( 100, 'plugin_hg:service_lbl_key', 'plugin_hg:service_desc_key', 'hg', '/plugins/hg/?group_id=$group_id', 1, 0, 'system', 230 );
 
 -- Create service for all other projects (but disabled)
 INSERT INTO service(group_id, label, description, short_name, link, is_active, is_used, scope, rank)
-  SELECT DISTINCT group_id, 'plugin_git:service_lbl_key', 'plugin_git:service_desc_key', 'git', CONCAT('/plugins/git/?group_id=', group_id), 1, 0, 'system', 230
+  SELECT DISTINCT group_id, 'plugin_hg:service_lbl_key', 'plugin_hg:service_desc_key', 'hg', CONCAT('/plugins/hg/?group_id=', group_id), 1, 0, 'system', 230
         FROM service
         WHERE group_id NOT IN (SELECT group_id
                                FROM service
                                WHERE short_name
-                               LIKE 'git');
+                               LIKE 'hg');
 
         
 INSERT INTO reference (id, keyword, description, link, scope, service_short_name, nature)
-VALUES (30, 'git', 'plugin_git:reference_commit_desc_key', '/plugins/git/index.php/$group_id/view/$1/?a=commit&h=$2', 'S', 'git', 'git_commit');
+VALUES (30, 'hg', 'plugin_hg:reference_commit_desc_key', '/plugins/hg/index.php/$group_id/view/$1/?a=commit&h=$2', 'S', 'hg', 'hg_commit');
 
 
 INSERT INTO reference_group (reference_id, group_id, is_active)
--- plugins/hg/db/uninstall.sql
+++ plugins/hg/db/uninstall.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS plugin_git;
+DROP TABLE IF EXISTS plugin_hg;
 
-DELETE FROM service WHERE short_name='git';
+DELETE FROM service WHERE short_name='hg';
 DELETE FROM reference_group WHERE reference_id=30;
 DELETE FROM reference WHERE id=30;
 
